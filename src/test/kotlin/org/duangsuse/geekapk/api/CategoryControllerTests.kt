56012b66092a90468cf66dd3ae83e7a26007cccb
duangsuse <fedora-opensuse@outlook.com>
2019-02-03 16:10:46 +0800

    Test(*): @ContextConfiguration

--- src/test/kotlin/org/duangsuse/geekapk/api/CategoryControllerTests.kt
+++ src/test/kotlin/org/duangsuse/geekapk/api/CategoryControllerTests.kt
@@ -9,12 +9,14 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@ContextConfiguration
 @RunWith(SpringRunner::class)
 @WebMvcTest(CategoryController::class)
 class CategoryControllerTests {
