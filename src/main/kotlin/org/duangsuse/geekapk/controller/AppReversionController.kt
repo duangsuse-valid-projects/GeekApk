6f125e6cd8deb121ecfaef190bdd6913c1d14ed5
duangsuse <fedora-opensuse@outlook.com>
2019-02-05 20:25:55 +0800

    Controller(AppReversion): Fix @RequestMapping - By replacing /appUpdate/ start context endpoint bind path

--- src/main/kotlin/org/duangsuse/geekapk/controller/AppReversionController.kt
+++ src/main/kotlin/org/duangsuse/geekapk/controller/AppReversionController.kt
@@ -14,35 +14,35 @@ class AppReversionController {
   @ResponseBody
   fun apiHint(hsr: HttpServletRequest) = ApiDoc.appUpdate(hsr).second
 
-  @GetMapping("/appUpdate/{aid}")
+  @GetMapping("/{aid}")
   @ResponseBody
   fun readReversions(@PathVariable("aid") aid: AppId): List<AppUpdate> {
     TODO()
   }
 
 
-  @GetMapping("/appUpdate/check/{aids}")
+  @GetMapping("/check/{aids}")
   @ResponseBody
   fun checkLastReversions(@PathVariable("aids") aids: String): List<Int> {
     TODO()
   }
 
 
-  @GetMapping("/appUpdate/{aid}/{rev}")
+  @GetMapping("/{aid}/{rev}")
   @ResponseBody
   fun readReversion(@PathVariable("aid") aid: AppId, @PathVariable("rev") rev: Int): AppUpdate {
     TODO()
   }
 
 
-  @PostMapping("/appUpdate/{aid}/{rev}")
+  @PostMapping("/{aid}/{rev}")
   @ResponseBody
   fun createReversion(@PathVariable("aid") aid: AppId, @PathVariable("rev") rev: Int): Map<String, Int> /* aid: number *//* rev: number */ {
     TODO()
   }
 
 
-  @PutMapping("/appUpdate/{aid}/{rev}")
+  @PutMapping("/{aid}/{rev}")
   @ResponseBody
   fun updateReversion(@PathVariable("aid") aid: AppId, @PathVariable("rev") rev: Int,
                       @RequestParam("attr") attr: String/* Maybe version or install or updates or minsdk */, @RequestBody value: String): Map<String, String> /* attr: string *//* oldVal: string */ {
@@ -50,7 +50,7 @@ class AppReversionController {
   }
 
 
-  @DeleteMapping("/appUpdate/{aid}/{rev}")
+  @DeleteMapping("/{aid}/{rev}")
   @ResponseBody
   fun dropReversion(@PathVariable("aid") aid: AppId, @PathVariable("rev") rev: Int): AppUpdate {
     TODO()
